a55c8814222afb2b0818dd0e82695d399e1e56f1
Ariouz <virgilou.c@gmail.com>
2020-04-22 15:48:31 +0200

    Added Plugin

--- src/fr/ariouz/npclib/NPCLib.java
+++ src/fr/ariouz/npclib/NPCLib.java
@@ -1,12 +1,22 @@
 package fr.ariouz.npclib;
 
 import fr.ariouz.npclib.npc.NPC;
+import org.bukkit.plugin.Plugin;
 
 import java.util.ArrayList;
 
 public class NPCLib {
 
     private ArrayList<NPC> npcs = new ArrayList<>();
+    private Plugin plugin;
+
+    public NPCLib(Plugin plugin){
+        this.plugin = plugin;
+    }
+
+    public Plugin getPlugin() {
+        return plugin;
+    }
 
     public ArrayList<NPC> getNpcs() {
         return npcs;
