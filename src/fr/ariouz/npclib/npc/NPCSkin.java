df42fb8bc1d966926d15e0be5fb668f6b205b38d
Ariouz <virgilou.c@gmail.com>
2020-04-24 19:02:55 +0200

    Added getSkinByPlayer method

--- src/fr/ariouz/npclib/npc/NPCSkin.java
+++ src/fr/ariouz/npclib/npc/NPCSkin.java
@@ -2,6 +2,8 @@ package fr.ariouz.npclib.npc;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
+import net.minecraft.server.v1_8_R3.EntityPlayer;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public class NPCSkin {
@@ -30,6 +32,14 @@ public class NPCSkin {
     }
 
     public void getSkinByPlayer(Player player){
+        EntityPlayer playerNMS = ((CraftPlayer) player).getHandle();
+        GameProfile profile = playerNMS.getProfile();
+        Property property = profile.getProperties().get("textures").iterator().next();
+        this.value = property.getValue();
+        this.signature = property.getSignature();
+    }
+
+    public void getSkinByPlayerName(String playerName){
 
     }
 
